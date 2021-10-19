d3a00a5ba0afcc117cc16e9bdc04649eaa8ab357
Murillo Muller <murillosmf@hotmail.com>
2021-10-19 07:36:40 -0300

    Converting Futures to async await

--- basic_app/lib/database/app_database.dart
+++ basic_app/lib/database/app_database.dart
@@ -2,40 +2,38 @@ import 'package:basic_app/models/project.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'projects.db');
-    return openDatabase(path, onCreate: (db, version) {
-      db.execute('CREATE TABLE projects('
-          'id INTEGER PRIMARY KEY, '
-          'title TEXT, '
-          'description TEXT)');
-    }, version: 1);
-  });
+Future<Database> createDatabase() async {
+  final String path = join(await getDatabasesPath(), 'projects.db');
+
+  return openDatabase(path, onCreate: (db, version) {
+    db.execute('CREATE TABLE projects('
+        'id INTEGER PRIMARY KEY, '
+        'title TEXT, '
+        'description TEXT)');
+  }, version: 1);
 }
 
-Future<int> saveProject(Project project) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> projectMap = {};
-    projectMap['title'] = project.title;
-    projectMap['description'] = project.description;
-    return db.insert('projects', projectMap);
-  });
+Future<int> saveProject(Project project) async {
+  final Database db = await createDatabase();
+  final Map<String, dynamic> projectMap = {};
+
+  projectMap['title'] = project.title;
+  projectMap['description'] = project.description;
+  return db.insert('projects', projectMap);
 }
 
-Future<List<Project>> findAllProjects() {
-  return createDatabase().then((db) {
-    return db.query('projects').then((maps) {
-      final List<Project> projects = [];
-      for (Map<String, dynamic> map in maps) {
-        final Project project = Project(
-          id: map['id'],
-          title: map['title'],
-          description: map['description'],
-        );
-        projects.add(project);
-      }
-      return projects;
-    });
-  });
+Future<List<Project>> findAllProjects() async {
+  final Database db = await createDatabase();
+  final List<Map<String, dynamic>> result = await db.query('projects');
+
+  final List<Project> projects = [];
+  for (Map<String, dynamic> row in result) {
+    final Project project = Project(
+      id: row['id'],
+      title: row['title'],
+      description: row['description'],
+    );
+    projects.add(project);
+  }
+  return projects;
 }
